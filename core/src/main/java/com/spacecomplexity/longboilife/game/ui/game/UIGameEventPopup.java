3fc0ef9a19f98fae7d07fbc5c2ec010150b35750
Gravy Jones <ojones_6tc@outlook.com>
2024-12-12 23:36:46 +0000

    Added UI for Game Events

--- core/src/main/java/com/spacecomplexity/longboilife/game/ui/game/UIGameEventPopup.java
+++ core/src/main/java/com/spacecomplexity/longboilife/game/ui/game/UIGameEventPopup.java
@@ -1,8 +1,13 @@
 package com.spacecomplexity.longboilife.game.ui.game;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.spacecomplexity.longboilife.game.gameevent.GameEvent;
+import com.spacecomplexity.longboilife.game.globals.GameEventManager;
 import com.spacecomplexity.longboilife.game.ui.UIElement;
 import com.spacecomplexity.longboilife.game.utils.EventHandler;
 
@@ -11,6 +16,8 @@ import com.spacecomplexity.longboilife.game.utils.EventHandler;
  */
 public class UIGameEventPopup extends UIElement {
     private final Skin skin;
+    private Label title;
+    private Label description;
 
     /**
      * Initialise event popup elements.
@@ -24,9 +31,29 @@ public class UIGameEventPopup extends UIElement {
 
         this.skin = skin;
 
+        // Initialise title label
+        title = new Label(null, skin);
+        title.setFontScale(1.5f);
+        title.setColor(Color.GREEN);
+
+        description = new Label(null, skin);
+        description.setFontScale(1f);
+        description.setColor(Color.WHITE);
+
+        // Place label onto table
+        table.add(title).align(Align.center);
+        table.row();
+        table.add(description).align(Align.center);
+
         // Style and place the table
         table.setBackground(skin.getDrawable("panel1"));
         placeTable();
+        closePopup();
+
+        EventHandler.getEventHandler().createEvent(EventHandler.Event.OPEN_GAMEEVENT_POPUP, (params) -> {
+            showPopup(GameEventManager.getGameEventManager().getCurrentGameEvent());
+            return null;
+        });
 
         EventHandler.getEventHandler().createEvent(EventHandler.Event.CLOSE_GAMEEVENT_POPUP, (params) -> {
             closePopup();
@@ -37,17 +64,24 @@ public class UIGameEventPopup extends UIElement {
     public void render() {
     }
 
+    public void showPopup(GameEvent gameEvent) {
+        title.setText(gameEvent.getType().getDisplayName());
+        description.setText(gameEvent.getType().getEventMessage());
+        table.setVisible(true);
+    }
+
     /**
      * Close the popup.
      */
-    public void closePopup() {
-        // Hide the build menu
+    private void closePopup() {
+        // Hide the popup
         table.setVisible(false);
     }
 
     @Override
     protected void placeTable() {
-        table.setSize(uiViewport.getWorldWidth(), 150);
-        table.setPosition(0, 55);
+        table.setSize(300, 60);
+        table.setPosition(uiViewport.getWorldWidth() - table.getWidth() - 180,
+                uiViewport.getWorldHeight() - table.getHeight());
     }
 }
