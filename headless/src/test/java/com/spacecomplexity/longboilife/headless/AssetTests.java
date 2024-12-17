d09d4ab9691e4c743282eb295f65c40e9521475f
DrWubenstein <lgsp2008@gmail.com>
2024-12-17 03:40:21 +0000

    Add test setup

--- headless/src/test/java/com/spacecomplexity/longboilife/headless/AssetTests.java
+++ headless/src/test/java/com/spacecomplexity/longboilife/headless/AssetTests.java
@@ -1,13 +1,9 @@
 package com.spacecomplexity.longboilife.headless;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.spacecomplexity.longboilife.game.building.BuildingType;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import  static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 public class AssetTests extends AbstractHeadlessGdxTest{
     // have to use direct file addresses as original group input data into methods and classes directly instead of using variables
