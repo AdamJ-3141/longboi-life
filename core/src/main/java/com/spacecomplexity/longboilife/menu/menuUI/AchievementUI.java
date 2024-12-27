92cd5357c0d94dee89b17ebe4d2702bc60adac27
Endorian36 <roryingram@icloud.com>
2024-12-27 13:20:57 +0000

    Changes achievementUI

--- core/src/main/java/com/spacecomplexity/longboilife/menu/menuUI/AchievementUI.java
+++ core/src/main/java/com/spacecomplexity/longboilife/menu/menuUI/AchievementUI.java
@@ -15,11 +15,11 @@ import com.spacecomplexity.longboilife.game.ui.UIElement;
 import com.spacecomplexity.longboilife.menu.MenuState;
 
 /**
- * Class that uses UIElement abstract class to create a display for the leaderboard
+ * Class that uses UIElement abstract class to create a display for the achievements
  */
 public class AchievementUI extends UIElement {
     /**
-     * Initialise Leaderboard window.
+     * Initialise achievement pop up window with the achievements being rendered
      *
      * @param uiViewport  the viewport used to render UI.
      * @param parentTable the table to render this element onto.
@@ -27,13 +27,18 @@ public class AchievementUI extends UIElement {
      */
     public AchievementUI(Viewport uiViewport, Table parentTable, Skin skin) {
         super(uiViewport, parentTable, skin);
-        table.debug();
-        Table[] achievementTables = new Table[AchievementType.values().length];
+
+        // creates achievement title label
+        Label achievementLabel = new Label("Achievements:", skin);
+        achievementLabel.setFontScale(1.2f);
+        table.add(achievementLabel).colspan(2).align(Align.center).padBottom(15);
+        table.row();
+
         for (AchievementType type : AchievementType.values()) {
             // Construct a new table for each achievement type.
-            achievementTables[type.ordinal()] = constructTable(type, skin);
+            Table achievementTable = constructTable(type, skin);
             // Add the new table to the main table.
-            table.add(achievementTables[type.ordinal()]);
+            table.add(achievementTable);
             // New row after every second table.
             if (type.ordinal() % 2 == 1) {
                 table.row();
@@ -51,18 +56,24 @@ public class AchievementUI extends UIElement {
             }
         });
 
+        //places the exit button
         table.row();
         table.add(exitButton).colspan(2).align(Align.center);
 
         // Style and place the table
         table.setBackground(skin.getDrawable("panel1"));
-        table.setSize(420, 380);
+        table.setSize(400, 380);
         placeTable();
     }
 
-    private static Table constructTable(AchievementType type, Skin skin) {
+    /**
+     * creates a formatted table for the achievement type passed too it
+     * @param type The achievement type
+     * @param skin The current skin
+     * @return Returns a Table containing the information about the achievement
+     */
+    private Table constructTable(AchievementType type, Skin skin) {
         Table currentTable = new Table();
-        currentTable.debug();
         Label tempTitleLabel = new Label(type.title, skin);
         Label tempDescriptionLabel = new Label(type.description, skin);
         Label tempScoreChangeLabel = new Label((type.scoreChange >= 0 ? "+" : "-") + type.scoreChange, skin);
@@ -71,23 +82,26 @@ public class AchievementUI extends UIElement {
         currentTable.add(tempDescriptionLabel);
         currentTable.row();
         currentTable.add(tempScoreChangeLabel);
+        currentTable.padLeft(15);
+        currentTable.padRight(15);
+        currentTable.padBottom(8);
         return currentTable;
     }
 
     /**
-     * renders the leaderboard window, updates the text if its currently being displayed
+     * renders the achievement window
      */
     public void render() {
         placeTable();
     }
 
     /**
-     * places table on screen if the leaderboard is currently being displayed
+     * places table on screen if the achievement is currently being displayed
      */
     @Override
     protected void placeTable() {
         if (MenuState.achievement) {
-            table.setPosition(110, 50);
+            table.setPosition(120, 50);
         }
         else {
             table.setPosition(uiViewport.getWorldWidth(), uiViewport.getWorldHeight());
